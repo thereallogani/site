8b1d5feacd294748ba6f8a3b3025c5805e05bd1c
Logan O'Hara <loganohara52@gmail.com>
2016-12-05 20:58:19 -0500

    Add error handling.

--- app.go
+++ app.go
@@ -3,16 +3,23 @@ package main
 import "net/http"
 import "html/template"
 import "site/page"
+import "log"
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *page.Page) {
-	t, _ := template.ParseFiles(tmpl + ".html")
+	t, err := template.ParseFiles(tmpl + ".html")
+	if err != nil {
+		log.Fatal(err)
+	}
 	t.Execute(w, p)
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
+func viewHandler(wr http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len(""):]
-	p, _ := page.LoadPage(title)
-	renderTemplate(w, "logan", p)
+	pg, err := page.LoadPage(title)
+	if err != nil {
+		log.Fatal(err)
+	}
+	renderTemplate(wr, "logan", pg)
 }
 
 func main() {
