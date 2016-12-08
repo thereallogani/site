473ca661415f197b097a6b33dc965cec28d39c69
Logan O'Hara <loganohara52@gmail.com>
2016-12-07 20:05:58 -0500

    Clean up code and switch to gin rather than net/http.

--- app.go
+++ app.go
@@ -1,50 +1,21 @@
 package main
 
 import (
-	"html/template"
-	"io/ioutil"
-	"log"
 	"net/http"
-	"site/page"
+
+	"github.com/gin-gonic/gin"
 )
 
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
-}
-
-func LoadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string, p *page.Page) {
-	t, err := template.ParseFiles(tmpl + ".html")
-	if err != nil {
-		log.Fatal(err)
-	}
-	t.Execute(w, p)
-}
-
-func viewHandler(wr http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len(""):]
-	pg, err := page.LoadPage(title)
-	if err != nil {
-		log.Fatal(err)
-	}
-	renderTemplate(wr, "logan", pg)
+func get_logan(c *gin.Context) {
+	c.HTML(http.StatusOK, "logan.html", gin.H{})
 }
 
 func main() {
-	http.HandleFunc("/", viewHandler)
-	http.ListenAndServe(":8080", nil)
+	router := gin.Default()
+
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/", get_logan)
+	gin.SetMode(gin.ReleaseMode)
+	router.Run("127.0.0.1:5000")
 }
